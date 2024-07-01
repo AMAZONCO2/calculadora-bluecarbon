595740e05371b6f22fb9e8c8d0cc0136e171a7b6
AMAZONCO2 <amazonconnectioncarbon@gmail.com>
2024-07-01 17:12:16 -0300

    adicionado suavização exponencial para prever o desmatamento

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ from modules.calcular_reducao import calcula_estoque, resultados_completos
 # Carregar projetos salvos do banco de dados
 def load_projects():
     conn = st.connection('postgresql', type="sql")
-    df = conn.query('SELECT * FROM carbono.projetos', ttl="10m")
+    df = conn.query('SELECT * FROM carbono.projetos', ttl="30s")
+    print("pesquisa no banco")
     project = pd.DataFrame(df)
     return project
 
@@ -48,6 +49,20 @@ def save_project(project_data):
         session.commit()
         project_id = result.fetchone()[0]
     return project_id
+
+# Atualizar nome do projeto no banco de dados
+def update_project_name(project_id, new_name):
+    conn = st.connection('postgresql', type="sql")
+    
+    query = text("""
+        UPDATE carbono.projetos
+        SET Nome_do_Projeto = :new_name
+        WHERE id = :project_id
+    """)
+    
+    with conn.session as session:
+        session.execute(query, {"new_name": new_name, "project_id": project_id})
+        session.commit()
     
 # Inicializar estado da sessão
 if 'projects' not in st.session_state:
@@ -55,28 +70,32 @@ if 'projects' not in st.session_state:
     
 # Salvar dados anuais no banco de dados
 def save_annual_data(project_id, annual_data: pd.DataFrame):
-    conn = st.connection('postgresql', type="sql")
-    
-    query = text("""
-        INSERT INTO carbono.resultados (Projeto_ID, Ano, Taxa_Desmatamento_Projetada, Linha_Base, Projeto_tco2, Vazamento, Buffer, Reducao_Liquida, VCUs_Acumulados)
-        VALUES (:projeto_id, :ano, :taxa_desmatamento_projetada, :linha_base, :projeto_tco2, :vazamento, :buffer, :reducao_liquida, :vcus_acumulados)
-    """)
-    
-    with conn.session as session:
-        for _, data in annual_data.iterrows():
-            session.execute(query, {
-                "projeto_id": project_id,
-                "ano": int(data["Ano"]),
-                "taxa_desmatamento_projetada": float(data["Taxa de Desmatamento Projetada (%)"]),
-                "linha_base": float(data["Linha Base (t CO2e)"]),
-                "projeto_tco2": float(data["Projeto (t CO2e)"]),
-                "vazamento": float(data["Vazamento (t CO2e)"]),
-                "buffer": float(data["Buffer (t CO2e)"]),
-                "reducao_liquida": float(data["Redução Líquida (t CO2e)"]),
-                "vcus_acumulados": float(data["VCUs Acumulados (t CO2e)"])
-            })
-        session.commit()
+    try:
+        conn = st.connection('postgresql', type="sql")
+        #print(annual_data)
+        query = text("""
+            INSERT INTO carbono.resultados (Projeto_ID, Ano, Taxa_Desmatamento_Projetada, Linha_Base, Projeto_tco2, Vazamento, Buffer, Reducao_Liquida, VCUs_Acumulados)
+            VALUES (:projeto_id, :ano, :taxa_desmatamento_projetada, :linha_base, :projeto_tco2, :vazamento, :buffer, :reducao_liquida, :vcus_acumulados)
+        """)
         
+        with conn.session as session:
+            for _, data in annual_data.iterrows():
+                session.execute(query, {
+                    "projeto_id": project_id,
+                    "ano": int(data["Ano"]),
+                    "taxa_desmatamento_projetada": float(data["Taxa de Desmatamento Projetada (%)"]),
+                    "linha_base": float(data["Linha Base (t CO2e)"]),
+                    "projeto_tco2": float(data["Projeto (t CO2e)"]),
+                    "vazamento": float(data["Vazamento (t CO2e)"]),
+                    "buffer": float(data["Buffer (t CO2e)"]),
+                    "reducao_liquida": float(data["Redução Líquida (t CO2e)"]),
+                    "vcus_acumulados": float(data["VCUs Acumulados (t CO2e)"])
+                })
+            session.commit()
+        print("ok - Enviado id: "+project_id)
+    except Exception as e:
+        print(f"Erro ao salvar dados anuais para o projeto {project_id}: {e}")
+    
 # Função para exibir a página de cadastro de projetos
 def cadastro_projeto():
     st.subheader("LINHA DE BASE")
@@ -102,7 +121,7 @@ def cadastro_projeto():
         estoque_total_inicial = delta_c_bsl
         
         # Calcular as reduções completas
-        reducoes = resultados_completos(estoque_total_inicial, fator_risco)
+        reducoes = resultados_completos(int(periodo_projeto),estoque_total_inicial, fator_risco, )
         
         # Dados do projeto
         project_data = {
@@ -121,7 +140,7 @@ def cadastro_projeto():
      
         # Salvar projeto e obter o ID
         project_id = save_project(project_data)
-        print(project_id)
+     
         # Salvar dados anuais
         save_annual_data(project_id, reducoes)
 
@@ -135,30 +154,45 @@ def cadastro_projeto():
         
 def relatorio():
     st.header("Relatório de Projetos")
+
     projects = st.session_state.projects
     resultados_lista = []
     if not projects.empty:
-        selected_projects = st.multiselect(
+        # Utilizar uma combinação de nome e ID para selecionar os projetos
+        project_options = {f"{row['nome_do_projeto']} (ID: {row['id']})": row['id'] for _, row in projects.iterrows()}
+        selected_project_keys = st.multiselect(
             "Selecione os projetos para visualizar no gráfico:",
-            projects['nome_do_projeto'].unique(), placeholder="Selecione uma opção"
+            project_options.keys(), placeholder="Selecione uma opção"
         )
-
-        if selected_projects:
-            filtered_projects = projects[projects['nome_do_projeto'].isin(selected_projects)]
-           
+     
+        if selected_project_keys:
+            selected_project_ids = [project_options[key] for key in selected_project_keys]
+            filtered_projects = projects[projects['id'].isin(selected_project_ids)]
+            
             for _, project in filtered_projects.iterrows():
                 #st.subheader(f"Projeto: {project['nome_do_projeto']}")
-              
+                #st.write(project['nome_do_projeto'])
                 projetos,resultados = load_select_project(project['id'])
-               
+                # Adicionar chave única para text_input e button
 
                 if not resultados.empty:   
                     st.subheader(f"Estimativa de 30 Anos para o Projeto: {project['nome_do_projeto']}")
-                    media_vcu_anual = resultados['vcus_acumulados'].sum() / len(resultados['vcus_acumulados'])
+                    key_prefix = str(project['id'])
+                    
+                    with st.expander(f"Editar nome do projeto"):
+                        new_name = st.text_input(f"Novo nome do projeto (ID: {project['nome_do_projeto']})", value=project['nome_do_projeto'], key=f"name_{key_prefix}")
+                        if st.button(f"Atualizar nome (ID: {project['id']})", key=f"button_{key_prefix}"):
+                            update_project_name(project['id'], new_name)
+                            st.session_state.projects = load_projects()
+                            st.success("Nome do projeto atualizado!")
+                    
+                    media_vcu_anual = resultados['reducao_liquida'].sum() / len(resultados['reducao_liquida'])
                     total_vcu = resultados['vcus_acumulados'].max()
                     buffer = resultados['buffer'].max()
-                    vazamento = resultados['vazamento'].max()
-                    anos = len(resultados['ano']) -1 
+                    vazamento = resultados['vazamento'].sum()
+                    anos = len(resultados['ano']) 
+                    linha_base = resultados['linha_base'].sum()
+                    projeto = resultados['projeto_tco2'].sum()
                     
                     with st.expander("Mostrar resumo"):
                         col1, col2 = st.columns(2)
@@ -169,8 +203,8 @@ def relatorio():
                             st.write(f"Área: {project['area']:.2f} ha")
                             st.write(f"Período do Projeto: {project['periodo_projeto']} anos")
                             st.write(f"Total de emissão (Vazamento) {anos} anos: {vazamento:.2f} tCO2e")
-                            st.write(f"Total de emissão (Linha de base) {anos} anos: {buffer:.2f} tCO2e")
-                            st.write(f"Total de emissão (Projeto) {anos} anos: {buffer:.2f} tCO2e")
+                            st.write(f"Total de emissão (Linha de base) {anos} anos: {linha_base:.2f} tCO2e")
+                            st.write(f"Total de emissão (Projeto) {anos} anos: {projeto:.2f} tCO2e")
 
                         with col2:
                             st.write(f"Fator de risco: {project['fator_de_risco']:.2f}")
@@ -282,15 +316,21 @@ if menu == "Cadastro de Projeto":
 elif menu == "Relatório":
     relatorio()
 
-# Mostrar projetos salvos
-st.sidebar.header("Projetos Salvos")
-st.session_state.projects['Projeto Display'] =  st.session_state.projects['nome_do_projeto']
-selected_project = st.sidebar.selectbox("Selecione um projeto", [""] + st.session_state.projects['Projeto Display'].tolist())
+# # Mostrar projetos salvos
+# st.sidebar.header("Projetos Salvos")
+# st.session_state.projects['Projeto Display'] =  st.session_state.projects['nome_do_projeto']
+# selected_project = st.sidebar.selectbox("Selecione um projeto", [""] + st.session_state.projects['Projeto Display'].tolist())
 
-if selected_project:
-    selected_id = (selected_project.split(" - ")[0])
-    project_data = st.session_state.projects[st.session_state.projects["id"] == selected_id]
-    st.sidebar.write(project_data)
+# if selected_project:
+#     selected_id = (selected_project.split(" - ")[0])
+#     project_data = st.session_state.projects[st.session_state.projects["id"] == selected_id]
+#     st.write(selected_project)
+#     # Campo para editar o nome do projeto
+#     new_name = st.sidebar.text_input("Novo nome do projeto", value=selected_project)
+#     if st.sidebar.button("Atualizar nome"):
+#         update_project_name(selected_id, new_name)
+#         st.session_state.projects = load_projects()
+#         st.sidebar.success("Nome do projeto atualizado!")
 
 #     st.sidebar.write("Resultados")
 #     st.sidebar.write(f"Emissões na Linha de Base (delta_c_bsl): {project_data['delta_c_bsl'].values[0]:.2f} tCO2e")
@@ -302,4 +342,4 @@ if selected_project:
 #     st.sidebar.write(f"Créditos de Carbono Anuais: {project_data['Créditos_Carbono_Anuais'].values[0]:.2f} tCO2e/ano")
 #     st.sidebar.write(f"Total do Valor dos Créditos Gerados ($): {project_data['Total_Valor_dos_Creditos_($)'].values[0]:.2f}")
 
-st.sidebar.markdown('<p style="font-size=5px">Desenvolvido por ACC © 2024 Amazon Connection Carbon. Todos os direitos reservados.</p>', unsafe_allow_html=True)
+st.sidebar.markdown('<p style="font-size=3px">Desenvolvido por ACC © 2024 Amazon Connection Carbon. Todos os direitos reservados.</p>', unsafe_allow_html=True)
--- modules/calcular_reducao.py
+++ modules/calcular_reducao.py
@@ -1,76 +1,85 @@
 import pandas as pd
+from statsmodels.tsa.api import ExponentialSmoothing
+from datetime import datetime
+
 # Dados históricos da coluna "Taxa de Desmatamento Acumulativa por Ano (%)"
 data_acumulativa = {
-    "Ano": [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022],
-    "Taxa de Desmatamento Acumulativa (%)": [0.97, 1.13, 1.13, 1.17, 1.17, 1.17, 1.70, 1.74, 1.83, 1.92, 1.92, 1.95, 1.98, 1.98, 1.98]
+    "Ano": [
+        2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015,
+        2016, 2017, 2018, 2019, 2020, 2021, 2022
+    ],
+    "Taxa de Desmatamento Acumulativa (%)": [
+        0.97, 1.13, 1.13, 1.17, 1.17, 1.17, 1.70, 1.74,
+        1.83, 1.92, 1.92, 1.95, 1.98, 1.98, 1.98
+    ]
 }
 
 # Criar DataFrame
 df_taxa_acumulativa = pd.DataFrame(data_acumulativa)
 
-# Aplicar suavização exponencial
-#model = ExponentialSmoothing(df_taxa_acumulativa['Taxa de Desmatamento Acumulativa (%)'], trend='add', seasonal=None)
-#fit = model.fit()
+def treinar_modelo(anos):
+    # Ajustar a suavização exponencial
+    model = ExponentialSmoothing(df_taxa_acumulativa['Taxa de Desmatamento Acumulativa (%)'], trend='add', seasonal=None)
+    fit = model.fit()
 
-# Projetar para os próximos 30 anos
-projection_years = list(range(2026, 2056 + 1))
-#projection = fit.forecast(len(projection_years))
-projection = [2.00,2.01,2.02,2.03,2.03,2.04,2.05,2.06,2.07,2.08,2.09,2.10,2.11,2.12,2.13,2.14,2.15,2.16,2.17,2.18,2.19,2.20,2.21,2.22,2.23,2.24,2.25,2.26,2.27,2.28,2.29]
-# Parâmetros utilizados
-
-
-def calcula_estoque(area,agb=0,bgb=0,dw=0,l=0,soc=0):
+    # Calcular o ano inicial (ano atual + 2)
+    ano_atual = datetime.now().year
+    ano_inicial = ano_atual + 2
+    projection_years = list(range(ano_inicial, ano_inicial + anos))
     
-    # Estoque total inicial
-    estoque_total_inicial = (agb+bgb+dw+l + soc) * area  # t C
+    # Projetar para os próximos anos
+    projection = fit.forecast(len(projection_years))
+
+    return projection_years, projection
+
+# Função para calcular o estoque inicial
+def calcula_estoque(area, agb=0, bgb=0, dw=0, l=0, soc=0):
+    estoque_total_inicial = (agb + bgb + dw + l + soc) * area  # t C
     return estoque_total_inicial
-    
-# Função para calcular as emissões e reduções com base na taxa de desmatamento anual projetada
-def calcular_reducoes(taxa_desmatamento, estoque_total_inicial, fator_risco ):
 
+# Função para calcular as emissões e reduções com base na taxa de desmatamento anual projetada
+def calcular_reducoes(taxa_desmatamento, estoque_total_inicial, fator_risco):
     conversao_para_CO2e = 3.6667  # Fator de conversão de carbono para CO2e
 
-    # Cálculo da perda anual de carbono
-   
     perda_carbono_anual = estoque_total_inicial * (taxa_desmatamento / 100)  # t C/ano
     perda_carbono_anual_CO2e = perda_carbono_anual * conversao_para_CO2e  # t CO2e/ano
-    
-    # Cálculo do vazamento
-    vazamento_anual = perda_carbono_anual_CO2e * 0.2 # t CO2e/ano
-    
-    # Cálculo do projeto de conservação
+
+    vazamento_anual = perda_carbono_anual_CO2e * 0.2  # t CO2e/ano
+
     projeto_conservacao = (perda_carbono_anual_CO2e - vazamento_anual) * 0.95  # t CO2e/ano
-    
-    # Cálculo do buffer
+
     buffer_anual = projeto_conservacao * fator_risco  # t CO2e/ano
-    
-    # Cálculo da redução anual ajustada
+
     reducao_anual_ajustada = projeto_conservacao - buffer_anual  # t CO2e/ano
-    
+
     return perda_carbono_anual_CO2e, projeto_conservacao, vazamento_anual, buffer_anual, reducao_anual_ajustada
 
-def resultados_completos(estoque_inicial,fator_risco,):
-    # Calculando as reduções para os próximos 30 anos
+# Função principal para calcular e retornar os resultados completos
+def resultados_completos(anos, estoque_inicial, fator_risco):
+    projection_years, projection = treinar_modelo(int(anos))
+
     resultados = []
     vcus_acumulados = 0
-
+    
     for ano, taxa in zip(projection_years, projection):
-        perda, conservacao, vazamento, buffer, reducao = calcular_reducoes(taxa, estoque_inicial, fator_risco)
+        
+        perda, conservacao, vazamento, buffer, reducao = calcular_reducoes(
+            taxa, estoque_inicial, fator_risco
+        )
         vcus_acumulados += reducao
-        resultados.append({
-            "Ano": int(ano),
-            "Taxa de Desmatamento Projetada (%)": taxa,
-            "Linha Base (t CO2e)": perda,
-            "Projeto (t CO2e)": conservacao,
-            "Vazamento (t CO2e)": vazamento,
-            "Buffer (t CO2e)": buffer,
-            "Redução Líquida (t CO2e)": reducao,
-            "VCUs Acumulados (t CO2e)": vcus_acumulados
-        })
+        
+        resultados.append(
+            {
+                "Ano": int(ano),
+                "Taxa de Desmatamento Projetada (%)": taxa,
+                "Linha Base (t CO2e)": perda,
+                "Projeto (t CO2e)": conservacao,
+                "Vazamento (t CO2e)": vazamento,
+                "Buffer (t CO2e)": buffer,
+                "Redução Líquida (t CO2e)": reducao,
+                "VCUs Acumulados (t CO2e)": round(vcus_acumulados,3),
+            }
+        )
 
-    # Criar DataFrame com os resultados
     df_resultados_completos = pd.DataFrame(resultados)
-    print(df_resultados_completos.dtypes)
-    # Exibir os resultados
-    #import ace_tools as tools; tools.display_dataframe_to_user(name="Tabela de VCU Completa com Projeção de Taxa de Desmatamento", dataframe=df_resultados_completos)
-    return(df_resultados_completos)
+    return df_resultados_completos
